fcaa4bd89816cb2ba7a5e9546566a58beb8c4b9a
vnayak-mishra <vinayakportfolio@gmail.com>
2025-10-07 21:29:01 +0530

    Changes in Background of Trending.jsx

--- frontend/src/pages/Trending.jsx
+++ frontend/src/pages/Trending.jsx
@@ -27,8 +27,7 @@ const Trending = () => {
   const navigate = useNavigate();
 
   return (
-    <section className="p-8 py-30 bg-cover bg-center"
-     style={{ backgroundImage: "url('/bg-img-1.png')" }} 
+    <section className="p-8 py-40 bg-slate-400/20" 
     >
       <h2 className="text-3xl mb-6 text-start font-cinzel bg-white inline-block  px-4 py-2
        drop-shadow-lg">
